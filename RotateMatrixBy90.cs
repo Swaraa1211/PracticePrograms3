76dea35e1f7807cdefc76f53017c6b2dd5413dc0
Swaraa1211 <saiswaroopasaranath1211@gmail.com>
2023-03-28 16:20:52 +0530

    Add project files.

---
+++ RotateMatrixBy90.cs
@@ -0,0 +1,39 @@
+﻿namespace PracticePrograms3
+{
+    internal class RotateMatrixBy90
+    {
+        static void Main(string[] args)
+        {
+           
+            int[,] arr = { { 1,2,3},
+                            {4,5,6 },
+                            {7,8,9 }};
+
+            rotateArr(arr);
+        }
+        public static void rotateArr(int[,] arr)
+        {
+            int[,] arrRotate = new int[3, 3];
+            int rows = 3, cols = 3, count = 0;
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j=0; j < cols; j++)
+                {
+                    arrRotate[j, rows-1 -count] = arr[i,j];
+                }
+                //cols--;
+                count++;
+
+
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(arrRotate[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
